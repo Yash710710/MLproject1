bf6b324780c0cf169ce7e9eb7863a62e5c4a73f4
Yash710710 <yashjain4284@gmail.com>
2025-04-14 20:20:31 +0530

    status

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+from typing import List
+def get_requirements(file_path:str)->List[str]:
+
+    hyphen_e_dot='-e .'
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+            return requirements
+setup(
+    name='mlproject1',
+    version='0.0.1',
+    author='YashJ',
+    author_email='yashjain4284@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+)
